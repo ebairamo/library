2a3c3fdab549d59464be61bd232ee7ac30356c84
ebairamo <napoleon5663@gmail.com>
2025-06-20 19:47:57 +0500

    ez

--- military/internal/ports/services/supply_service.go
+++ military/internal/ports/services/supply_service.go
@@ -1,1 +1,11 @@
 package services
+
+import "military/internal/domain"
+
+type SupplyService interface {
+	OrderSupply(supplierName string, products map[int]int) (*domain.Supply, error)
+	ChangeToDelivered(supplyID int) error
+	CancelSupply(supplyID int) error
+	GetPendingList() ([]*domain.Supply, error)
+	AddToWarehouse(supplyID int) error
+}
